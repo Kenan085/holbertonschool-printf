0e5dd4738b44bff7c7a26393b3203f538ff0ea3f
Kenan <kenanshafizade85@gmail.com>
2024-03-27 10:30:24 +0000

    Added print_int function and solved betty errors

--- functions.c
+++ functions.c
@@ -10,5 +10,51 @@
 
 int _putchar (char c)
 {
-	return write (1,&c, 1);
+	return (write(1, &c, 1));
+}
+
+/**
+ * print_int
+ * @s: our integer variable
+ *
+ * Return: count
+ */
+
+int print_int (int s)
+{
+	int n;
+	int rem, i = 0;
+	char number[25];
+
+	if (s < 0)
+	{
+		_putchar ('-');
+		n = -1 * s;
+	}
+	else
+	{
+		n = s;
+	}
+	if (n == 0)
+	{
+		_putchar (48);
+		return (0);
+	}
+	while (n > 0)
+	{
+		rem = n % 10;
+		number[i] = rem + 48;
+		n = n / 10;
+		if (n == 0)
+		{
+			break;
+		}
+		i++;
+	}
+	while (i >= 0)
+	{
+		_putchar (number[i]);
+		i--;
+	}
+	return (1);
 }
