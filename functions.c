66c397ccedacfcc2ef6037765e675a8f997b91ff
Pakizamutallim <62bq5tn@code.edu.az>
2024-03-27 10:07:17 +0000

    deleted pakiza branch

---
+++ functions.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * _putchar - prints character
+ *
+ * @c: it is a char variable
+ *
+ * Return: return 0.
+ */
+
+int _putchar (char c)
+{
+	return write (1,&c, 1);
+}
