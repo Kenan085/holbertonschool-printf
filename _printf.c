e88e1aba31a49b3048a4de9110b2848706867965
Kenan <kenanshafizade85@gmail.com>
2024-03-28 10:51:16 +0000

    Updated _printf.c

--- _printf.c
+++ _printf.c
@@ -18,6 +18,49 @@ int _printf(const char *format, ...)
 	va_start(args, format);
 	while (*(format + i) != '\0')
 	{
-		if (
+		if (*(format + i) != '%')
+		{
+			_putchar (*(format + i));
+			n++;
+		}
+		else if (*(format + i) == '%' && *(format + i + 1) == NULL)
+		{
+			continue;
+		}
+		else if (*(format + i) == '%' && *(format + i + 1) == '%')
+		{
+			_putchar ('%');
+			n++;
+			i++;
+		}
+		else if (*(format + i) == '%' && *(format + i + 1) == 'c')
+		{
+			n = print_char(n, va_arg(args, int));
+			i++;
+		}
+		else if (*(format + i) == '%' && *(format + i + 1) == 's')
+		{
+			n = print_str(n, va_arg(args, char *));
+			i++;
+		}
+		else if (*(format + i) == '%' && *((format + i + 1) == 'd')
+					|| (*(format + i + 1) == 'i'))
+		{
+			n = print_int(n, va_arg(args, int));
+			i++;
+		}
+		else
+		{
+			_putchar(*(format + i));
+			n++;
+		}
+
+		i++;
 	}
+	va_end(args);
+	if (n == 0)
+	{
+		n = -1;
+	}
+	return (n);
 }
